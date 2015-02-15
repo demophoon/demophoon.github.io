6d4981756604818603cbd739cf2c419ff087f792
Britt Gresham <britt@puppetlabs.com>
2015-02-15 10:23:08 -0800

    Fix http absolute paths
    Before this commit when a page was generated it was using the http
    endpoint instead of the https endpoint which causes github to render the
    page strangely in browsers with Content Security Policy rules enabled.

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://demophoon.github.io/'
+SITEURL = 'https://demophoon.github.io/'
 RELATIVE_URLS = False
 
 FEED_DOMAIN = "https://demophoon.github.io"
